0f08dd0ccb63062f0f49c7e2c2435b6523de3bd2
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-10-22 07:29:50 -0300

    Pronto para testes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from BSInterpreter import BSInterpreter
 
+print("***************************")
+print("* BS Programming Language *")
+print("*       by Rafael Silva   *")
+print("***************************")
 while True:
-    path = input("Caminho do arquivo .bs: ")
+    path = input("Caminho do arquivo .BS: ")
     interpreter = BSInterpreter(path)
     # interpreter.SaveToJson()
     print(f"{path}:")
